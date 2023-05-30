c9585c85009e6263fc6ccf3521efa806760be68f
Alecs <erick.borges@outlook.es>
2023-05-29 22:43:42 -0600

    HomeScreen started

---
+++ components/Styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    header: {
+        fontSize: 50,
+        fontWeight: 800,
+        color: '#000',
+    },
+
+    text: {
+        fontSize: 18,
+        fontWeight: 300,
+        color: '#000'
+    },
+})
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,13 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import Styles from '../components/Styles';
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
-  )
+    return (
+        <View>
+            <View style={styles.maintxtView}>
+                <Text style={Styles.header}>Hello User,</Text>
+                <Text style={Styles.text}>Good morning.</Text>
+            </View>
+        </View>
+    )
 }
 
 export default HomeScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1C1C1E',
+    },
+
+    maintxtView: {
+        paddingTop: 50,
+        paddingLeft: 40,
+        paddingBottom: 10
+    },
+
+    todayTextView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 30,
+        marginLeft: 40,
+        marginRight: 40,
+    },
+
+});
